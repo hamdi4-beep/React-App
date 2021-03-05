9d294bcce7a0d8e7d00ebb05744cc5acef2f3635
hamdi-beep4 <git@github.com>
2021-03-05 11:21:53 -0800

    added new links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import Navbar from './Navbar';
+import Create from './Create';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
         <Navbar></Navbar>
-        <Home></Home>
-    </div>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/create">
+              <Create></Create>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
